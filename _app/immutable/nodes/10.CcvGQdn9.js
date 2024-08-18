701aca79c44ba2e992a98d7d27bbc7c7f1eb1595
satrialoka <satrialoka@gmail.com>
2024-08-18 20:47:54 +0300

    build new

---
+++ _app/immutable/nodes/10.CcvGQdn9.js
@@ -0,0 +1,4 @@
+import{s as u,n as o}from"../chunks/scheduler.qzY_7jFv.js";import{S as d,i as _,e as m,s as C,a as h,g as p,o as x,d as l,f as i}from"../chunks/index.DPHJD2rk.js";function v(f){let e,r="🚧 Under Construction 🚧",s,n,c=`<h2>Selected Publication</h2>
+    here you can find my profesional works
+
+    <h2>Codes</h2> <h2>Projects</h2>`;return{c(){e=m("h1"),e.textContent=r,s=C(),n=m("article"),n.innerHTML=c},l(t){e=h(t,"H1",{"data-svelte-h":!0}),p(e)!=="svelte-7917jp"&&(e.textContent=r),s=x(t),n=h(t,"ARTICLE",{"data-svelte-h":!0}),p(n)!=="svelte-1rw65ry"&&(n.innerHTML=c)},m(t,a){l(t,e,a),l(t,s,a),l(t,n,a)},p:o,i:o,o,d(t){t&&(i(e),i(s),i(n))}}}class L extends d{constructor(e){super(),_(this,e,null,v,u,{})}}export{L as component};
