b4e2a1093d8494daa992452a1d679cc8df625998
satrialoka <satrialoka@gmail.com>
2024-08-18 20:53:31 +0300

    build-new

---
+++ build/_app/immutable/entry/start.Dw8Ogcdu.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DtiQqVPx.js";export{t as start};
---
+++ build/_app/immutable/nodes/3.BK_fUqvF.js
@@ -0,0 +1,7 @@
+import{s,n as t}from"../chunks/scheduler.qzY_7jFv.js";import{S as o,i as l,e as h,a as p,g as c,d as m,f as g}from"../chunks/index.DPHJD2rk.js";function u(n){let e,i=`<h3>Hi welcome to my blog</h3> <p>I am a postdoctoral <a href="https://scholar.google.co.id/citations?user=bA9vjiAAAAAJ&amp;hl=en" target="_blank">researcher</a>
+      at <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a> in the University of Helsinki, Finland. 
+      My work focuses on Bayesian optimization, deep learning, and amortized (inference) optimization.
+      Previously, I pursued my PhD research in the <a href="https://sumo.ilabt.imec.be/home" target="_blank">SUMO group</a> at Ghent University in Belgium, 
+      specializing in Bayesian Optimization for engineering applications. Prior to that, 
+      I completed my master&#39;s degree at <a href="https://kurita-lab.jp/wordpress/" target="_blank">Kurita Laboratory</a>, Hiroshima University in Japan, 
+      where my focus was on Deep Neural Networks.</p> <br/>`;return{c(){e=h("article"),e.innerHTML=i},l(a){e=p(a,"ARTICLE",{"data-svelte-h":!0}),c(e)!=="svelte-i0pdeg"&&(e.innerHTML=i)},m(a,r){m(a,e,r)},p:t,i:t,o:t,d(a){a&&g(e)}}}class y extends o{constructor(e){super(),l(this,e,null,u,s,{})}}export{y as component};
