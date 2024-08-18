1586ca182cfe2d5aaffc3a66a68247e02e0a8730
satrialoka <satrialoka@gmail.com>
2024-08-18 20:33:38 +0300

    init

---
+++ _app/immutable/nodes/3.TvR5SR2y.js
@@ -0,0 +1,7 @@
+import{s,n as t}from"../chunks/scheduler.cnANAmfW.js";import{S as o,i as l,g as h,h as c,x as m,a as p,f as u}from"../chunks/index.8wAQd9os.js";function g(n){let e,i=`<h3>Hi welcome to my blog</h3> <p>I am a postdoctoral <a href="https://scholar.google.co.id/citations?user=bA9vjiAAAAAJ&amp;hl=en" target="_blank">researcher</a>
+      at <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a> in the University of Helsinki Finland. 
+      My work focuses on Bayesian optimization, deep learning, and amortized (inference) optimization.
+      Previously, I pursued my PhD research in the <a href="https://sumo.ilabt.imec.be/home" target="_blank">SUMO group</a> at Ghent University in Belgium, 
+      specializing in Bayesian Optimization for engineering applications. Prior to that, 
+      I completed my master&#39;s degree at <a href="https://kurita-lab.jp/wordpress/" target="_blank">Kurita Laboratory</a>, Hiroshima University in Japan, 
+      where my focus was on Deep Neural Networks.</p> <br/>`;return{c(){e=h("article"),e.innerHTML=i},l(a){e=c(a,"ARTICLE",{"data-svelte-h":!0}),m(e)!=="svelte-1g1u2ek"&&(e.innerHTML=i)},m(a,r){p(a,e,r)},p:t,i:t,o:t,d(a){a&&u(e)}}}class y extends o{constructor(e){super(),l(this,e,null,g,s,{})}}export{y as component};
