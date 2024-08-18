084d412d041d205c616e448adfc7376e6caaf06f
satrialoka <satrialoka@gmail.com>
2024-08-18 20:52:59 +0300

    coma

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,7 +3,7 @@
 <article>
    <h3>Hi welcome to my blog</h3>
    <p> I am a postdoctoral <a href="https://scholar.google.co.id/citations?user=bA9vjiAAAAAJ&hl=en" target="_blank">researcher</a>
-      at <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a> in the University of Helsinki Finland. 
+      at <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a> in the University of Helsinki, Finland. 
       My work focuses on Bayesian optimization, deep learning, and amortized (inference) optimization.
       Previously, I pursued my PhD research in the <a href="https://sumo.ilabt.imec.be/home" target="_blank">SUMO group</a> at Ghent University in Belgium, 
       specializing in Bayesian Optimization for engineering applications. Prior to that, 
