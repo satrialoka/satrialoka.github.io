3b4706f21f6c20d0df3d1b90bc6c2e32d4d8aa63
satrialoka <satrialoka@gmail.com>
2024-08-20 21:08:54 +0300

    rewrite about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,13 +1,13 @@
 <!-- about/+page.svelte -->
 
 <article>
-   <h3>Hi welcome to my blog</h3>
+   <h3>Hi welcome to site</h3>
    <p> I am a postdoctoral <a href="https://scholar.google.co.id/citations?user=bA9vjiAAAAAJ&hl=en" target="_blank">researcher</a>
-      at <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a> in the University of Helsinki, Finland. 
-      My work focuses on Bayesian optimization, deep learning, and amortized (inference) optimization.
-      Previously, I pursued my PhD research in the <a href="https://sumo.ilabt.imec.be/home" target="_blank">SUMO group</a> at Ghent University in Belgium, 
-      specializing in Bayesian Optimization for engineering applications. Prior to that, 
+      in <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a>, with <a href="https://lacerbi.github.io/">Luigi Acerbi</a>, at the University of Helsinki, Finland. 
+      My work focuses on amortized inference methods and their applications, especially in black-box optimization. My research interests include Bayesian optimization, (deep) neural networks, and their applications in science and engineering.
+      Previously, I pursued my PhD in the <a href="https://sumo.ilabt.imec.be/home" target="_blank">SUMO group</a> at Ghent University in Belgium, 
+      specializing in Bayesian optimization for engineering applications. Prior to that, 
       I completed my master's degree at <a href="https://kurita-lab.jp/wordpress/" target="_blank">Kurita Laboratory</a>, Hiroshima University in Japan, 
-      where my focus was on Deep Neural Networks.</p>
+      where my focus was on Deep Neural Networks. You can download my CV <a href="/files/CV_Loka.pdf">here</a>.</p>
    <br>
 </article>
