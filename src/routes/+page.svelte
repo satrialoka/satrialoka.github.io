c14950432e3a7d5c8732bf52a2a004fd83460079
satrialoka <satrialoka@gmail.com>
2024-08-20 20:35:55 +0300

    front page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-Hi welcome! see <a href="\about">about</a>.
+<h1>
+    <span style="font-weight: normal;">Nasrulloh Ratu Bagus Satrio</span> 
+    <span style="font-weight: bold;">Loka</span>
+</h1>
+
+<div class="content-container">
+    <div class="image-container">
+        <img src="images/loka-casual-pic.jpg" alt="Descriptive Alt Text">
+    </div>
+    <div class="text-container">
+        Hello, I'm Loka, a Postdoctoral Researcher in <a href="https://lacerbi.github.io/">Luigi Acerbi</a>'s Lab at the University of Helsinki, Finland. I occasionally write about machine learning and a variety of other interests on my <a href="/blog">blog</a>. For more information about my background and previous positions, please visit the <a href="/about">about</a> page.
+    </div>
+</div>
