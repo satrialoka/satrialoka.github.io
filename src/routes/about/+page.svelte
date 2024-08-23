b5fb19c7b9f7abccac93d03ab105e012473363d1
satrialoka <satrialoka@gmail.com>
2024-08-23 19:59:40 +0300

    build-23-06-24

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,9 @@
 <!-- about/+page.svelte -->
 
 <article>
-   <h3>Hi welcome to site</h3>
+   <h3>Hi welcome!</h3>
    <p> I am a postdoctoral <a href="https://scholar.google.co.id/citations?user=bA9vjiAAAAAJ&hl=en" target="_blank">researcher</a>
-      in <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence research group</a>, with <a href="https://lacerbi.github.io/">Luigi Acerbi</a>, at the University of Helsinki, Finland. 
+      in <a href="https://www.helsinki.fi/en/researchgroups/machine-and-human-intelligence" target="_blank">The Machine and Human Intelligence Lab</a>, with <a href="https://lacerbi.github.io/">Luigi Acerbi</a>, at the University of Helsinki, Finland. 
       My work focuses on amortized inference methods and their applications, especially in black-box optimization. My research interests include Bayesian optimization, (deep) neural networks, and their applications in science and engineering.
       Previously, I pursued my PhD in the <a href="https://sumo.ilabt.imec.be/home" target="_blank">SUMO group</a> at Ghent University in Belgium, 
       specializing in Bayesian optimization for engineering applications. Prior to that, 
