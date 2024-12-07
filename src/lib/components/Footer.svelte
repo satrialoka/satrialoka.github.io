a99fdae449a8306d0018fd7636bfdda517eba74f
satrialoka <satrialoka@gmail.com>
2024-12-07 20:34:01 +0200

    update font awesome, add more links to social acc

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,21 @@
 <!-- Footer.svelte -->
 <footer>
     <div class="social-links">
-      <a href="https://twitter.com/nasloka" target="_blank" rel="noopener" aria-label="Twitter">
-        <i class="fab fa-twitter"></i>
+
+      <a href="mailto:satrialoka@gmail.com" target="_blank" rel="noopener" aria-label="Email">
+        <i class="fas fa-envelope"></i>
+      </a>
+      <a href="https://scholar.google.com/citations?user=bA9vjiAAAAAJ&hl=en" target="_blank" rel="noopener" aria-label="Google Scholar">
+        <i class="fa-solid fa-graduation-cap"></i>
       </a>
       <a href="https://github.com/satrialoka" target="_blank" rel="noopener" aria-label="GitHub">
         <i class="fab fa-github"></i>
       </a>
-      <a href="mailto:satrialoka@gmail.com" target="_blank" rel="noopener" aria-label="Email">
-        <i class="fas fa-envelope"></i>
+      <a href="https://twitter.com/nasloka" target="_blank" rel="noopener" aria-label="Twitter">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="https://bsky.app/profile/nasrullohloka.bsky.social" target="_blank" rel="noopener" aria-label="Blue Sky">
+        <i class="fa-brands fa-bluesky"></i>
       </a>
     </div>
     <p>&copy; 2024 Nasrulloh Loka.</p>
