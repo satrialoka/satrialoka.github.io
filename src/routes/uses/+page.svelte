74681b8b4a98a5b951b5edb02cdf574bf9249be9
satrialoka <satrialoka@gmail.com>
2025-01-19 15:02:18 +0200

    add uses

---
+++ src/routes/uses/+page.svelte
@@ -0,0 +1,41 @@
+<!-- uses/+page.svelte -->
+
+<h1>Uses</h1>
+My current technology setup, inspired by <a href="https://uses.tech/">uses.tech</a>
+
+<h3>Hardwares</h3>
+<ul>
+    <li>Laptop: Macbook Air M2, Thinkpad with <a href="https://helpdesk.it.helsinki.fi/en/instructions/other-instructions/cubbli-university-helsinki">Cubbli</a><sup>*</sup></li>
+    <li>HPC: Puhti<sup>*</sup>, LUMI<sup>*</sup> </li>
+    <li>Mobile: iPad 2018, Google Pixel 8 Pro</li>
+    <li>IO: Logitech G-pro Mouse, Xbox controller, Jabra Elite Active 75t</li>
+</ul>
+*provided by the university
+
+<h3>Development tools</h3>
+<ul>
+    <li>Terminal: Ghostty</li>
+    <li>Editor: Visual Studio Code, NeoVIM with nvimkickstart</li>
+    <li>Language: Python</li>
+    <li>ML stuff: Pytorch, Tensorflow, NumPy, SciPy</li>
+    <li>Web: Svelte and JS</li>
+    <li>Misc: LaTeX, GIT</li>
+</ul>
+
+<h3>Research and Writing apps</h3>
+<ul>
+    <li>Writing: Notion, Obsidian, Overleaf, Google Docs</li>
+    <li>Research: Zotero (with koffr storage)</li>
+</ul>
+
+<h3>Sports</h3>
+<ul>
+    <li>Watches: Garmin Venu 2</li>
+    <li>Sports apps: Strava, Garmin Connect</li>
+</ul>
+
+<h3>No longer used / old tech stack</h3>
+<ul>
+    <li>Language: C#, Java, C</li>
+    <li>Terminal: iTerm</li>
+</ul>
